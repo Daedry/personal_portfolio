73562e4f1b3e1398720ac73b69909dd749699665
MyUsername <romamargine@gmail.com>
2022-08-08 18:45:32 +0200

    Add logic for fileter porfolio items

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,82 @@
 /*----------------------- about section tabs -----------------------*/
 
 (() => {
-    const aboutSection = document.querySelector('.about-section');
-    const tabsContainer = document.querySelector('.about-tabs');
+  const aboutSection = document.querySelector(".about-section");
+  const tabsContainer = document.querySelector(".about-tabs");
 
-    tabsContainer.addEventListener('click', (e) => {
-        if(e.target.classList.contains('tab-item') && !e.target.classList.contains('active')){
-         const target = e.target.getAttribute('data-target');
-        //  deactivate existing active 'tab-item'
-        tabsContainer.querySelector('.active').classList.remove('outer-shadow', 'active');
-        // activate new 'tab-item'
-        e.target.classList.add('outer-shadow', 'active');
-        // deactivate existing active 'tab-content'
-        aboutSection.querySelector('.tab-content.active').classList.remove('active');
-        // activate new 'tab-content'
-        aboutSection.querySelector(target).classList.add('active');
+  tabsContainer.addEventListener("click", (e) => {
+    if (
+      e.target.classList.contains("tab-item") &&
+      !e.target.classList.contains("active")
+    ) {
+      const target = e.target.getAttribute("data-target");
+      //  deactivate existing active 'tab-item'
+      tabsContainer
+        .querySelector(".active")
+        .classList.remove("outer-shadow", "active");
+      // activate new 'tab-item'
+      e.target.classList.add("outer-shadow", "active");
+      // deactivate existing active 'tab-content'
+      aboutSection
+        .querySelector(".tab-content.active")
+        .classList.remove("active");
+      // activate new 'tab-content'
+      aboutSection.querySelector(target).classList.add("active");
+    }
+  });
+})();
 
+/*----------------------- stop scrolling-y -----------------------*/
+
+function bodyScrollingToggle() {
+  document.body.classList.toggle("stop-scrolling");
+}
+
+/*----------------------- portfolio filter and popup -----------------------*/
+
+(() => {
+  const filterCointainer = document.querySelector(".portfolio-filter"),
+    portfolioItemsContainer = document.querySelector(".portfolio-items"),
+    portfolioItems = document.querySelectorAll(".portfolio-item"),
+    popup = document.querySelector(".portfolio-popup"),
+    prevBtn = popup.querySelector(".pp-prev"),
+    nextBtn = popup.querySelector(".pp-next"),
+    closeBtn = popup.querySelector(".pp-close"),
+    projectDetailsContainer = popup.querySelector(".pp-details"),
+    projectDetailsBtn = document.querySelector(".pp-project-details-btn");
+  let itemIndex, slideIndex, screenshots;
+
+  // filter portfolio items
+  filterCointainer.addEventListener("click", (e) => {
+    if (
+      e.target.classList.contains("filter-item") &&
+      !e.target.classList.contains("active")
+    ) {
+      // deactivate existing active 'filter-item'
+      filterCointainer
+        .querySelector(".active")
+        .classList.remove("outer-shadow", "active");
+
+      // activate new 'filter-item'
+      e.target.classList.add("outer-shadow", "active");
+
+      // get filter value
+      const target = e.target.getAttribute("data-target");
+
+      // filter portfolio items
+      portfolioItems.forEach((item) => {
+        if (target === item.getAttribute("data-category") || target === "all") {
+          item.classList.remove("hide");
+          item.classList.add("show");
+        } else {
+          item.classList.remove("show");
+          item.classList.add("hide");
         }
-    });
+      });
+    }
+  });
+
+  
+
 
 })();
