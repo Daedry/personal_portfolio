fc086b0e3148af22b1f6860bf9f95ec409dce7dc
MyUsername <romamargine@gmail.com>
2022-08-11 17:57:48 +0200

    Add logic for nav-menu and links

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,80 @@
+/*----------------------- navigation menu -----------------------*/
+
+(() => {
+  const hamburgerBtn = document.querySelector(".hamburger-btn"),
+  navMenu = document.querySelector(".nav-menu"),
+  closeNavBtn = navMenu.querySelector(".close-nav-menu");
+
+  hamburgerBtn.addEventListener("click", showNavMenu);
+  closeNavBtn.addEventListener("click", hideNavMenu);
+
+  function showNavMenu() {
+    navMenu.classList.add("open");
+    bodyScrollingToggle();
+  }
+
+  function hideNavMenu() {
+    navMenu.classList.remove("open");
+    fadeOutEffect();
+    bodyScrollingToggle();
+  }
+
+  function fadeOutEffect() {
+    document.querySelector(".fade-out-effect").classList.add("active");
+    setTimeout(() => {
+      document.querySelector(".fade-out-effect").classList.remove("active");
+    }, 370);
+  }
+
+  // attach an event handler to document
+  document.addEventListener("click", (event) => {
+    // console.log(event.target.hash);
+    if (event.target.classList.contains("link-item")) {
+      // make sure event.target.hash has a value before overridding default behavior
+      if (event.target.hash !== "") {
+        // prevent defaut anchor click behavior
+        event.preventDefault();
+        const hash = event.target.hash;
+
+        // deactive existing active section
+        document.querySelector(".section.active").classList.add("hide");
+        document.querySelector(".section.active").classList.remove("active");
+
+        // activate new section
+        document.querySelector(hash).classList.add("active");
+        document.querySelector(hash).classList.remove("hide");
+
+        // deactivate existing active navigation menu 'link-item'
+        navMenu.querySelector(".active").classList.add("outer-shadow", "hover-in-shadow");
+        navMenu.querySelector(".active").classList.remove("active", "inner-shadow");
+
+        //if clicked 'link-item' is contained withing the navigation menu
+        if(navMenu.classList.contains("open")){
+          // activate new navigation menu 'link-item'
+          event.target.classList.add("active", "inner-shadow");
+          event.target.classList.remove("outer-shadow", "hover-in-shadow");
+
+          // hide mnavigation menu
+          hideNavMenu();
+        } else {
+          let navItems = navMenu.querySelectorAll(".link-item");
+          navItems.forEach((item) => {
+            if(hash === item.hash){
+              // activate new navigation menu 'link-item'
+            item.classList.add("active", "inner-shadow");
+            item.classList.remove("outer-shadow", "hover-in-shadow");
+            }
+          });
+          fadeOutEffect();
+        }
+
+        // add hash (#) to URL
+        window.location.hash = hash;
+      }
+    }
+  });
+})();
+
 /*----------------------- about section tabs -----------------------*/
 
 (() => {
@@ -119,7 +196,7 @@ function bodyScrollingToggle() {
     popupToggle();
 
     // reset details popup
-    if(projectDetailsContainer.classList.contains("active")){
+    if (projectDetailsContainer.classList.contains("active")) {
       popupDetailsToggle();
     }
   });
@@ -163,31 +240,37 @@ function bodyScrollingToggle() {
 
   function popupDetails() {
     // if portfolio-item-details not exists
-    if(!portfolioItems[itemIndex].querySelector(".portfolio-item-details")) {
+    if (!portfolioItems[itemIndex].querySelector(".portfolio-item-details")) {
       projectDetailsBtn.style.display = "none";
       return;
     }
     projectDetailsBtn.style.display = "block";
 
     // get project details
-    const details = portfolioItems[itemIndex].querySelector(".portfolio-item-details").innerHTML;
+    const details = portfolioItems[itemIndex].querySelector(
+      ".portfolio-item-details"
+    ).innerHTML;
 
     // set project details
     popup.querySelector(".pp-project-details").innerHTML = details;
 
     // get the project title
-    const title = portfolioItems[itemIndex].querySelector(".portfolio-item-title").innerHTML;
+    const title = portfolioItems[itemIndex].querySelector(
+      ".portfolio-item-title"
+    ).innerHTML;
 
     // set the project title
     popup.querySelector(".pp-title h2").innerHTML = title;
-    
+
     // get the project category
     const category = portfolioItems[itemIndex].getAttribute("data-category");
 
     // set the project category
-    popup.querySelector(".pp-project-category").innerHTML = category.split("-").join(" ");;  
+    popup.querySelector(".pp-project-category").innerHTML = category
+      .split("-")
+      .join(" ");
   }
- 
+
   // show project details
   projectDetailsBtn.addEventListener("click", () => {
     return popupDetailsToggle();
@@ -219,35 +302,32 @@ function bodyScrollingToggle() {
       // popup.scrollTo(0,projectDetailsContainer.offsetTop);
     }
   }
-
-
 })();
-
-
-
 
 /*----------------------- testimonial slider -----------------------*/
 
 (() => {
   const sliderContainer = document.querySelector(".testi-slider-container"),
-  slides = sliderContainer.querySelectorAll(".testi-item"),
-  slideWidth = sliderContainer.offsetWidth,
-  prevBtn = document.querySelector(".testi-slider-nav .prev"),
-  nextBtn = document.querySelector(".testi-slider-nav .next"),
-  activeSlide = sliderContainer.querySelector(".testi-item.active");
-  let slideIndex = Array.from(activeSlide.parentElement.children).indexOf(activeSlide);
+    slides = sliderContainer.querySelectorAll(".testi-item"),
+    slideWidth = sliderContainer.offsetWidth,
+    prevBtn = document.querySelector(".testi-slider-nav .prev"),
+    nextBtn = document.querySelector(".testi-slider-nav .next"),
+    activeSlide = sliderContainer.querySelector(".testi-item.active");
+  let slideIndex = Array.from(activeSlide.parentElement.children).indexOf(
+    activeSlide
+  );
 
   // set width of all slides
   slides.forEach((slide) => {
     slide.style.width = slideWidth + "px";
-  })
+  });
 
   // set width of sliderContainer
   sliderContainer.style.width = slideWidth * slides.length + "px";
 
   // next slide
   nextBtn.addEventListener("click", () => {
-    if(slideIndex === slides.length - 1) {
+    if (slideIndex === slides.length - 1) {
       slideIndex = 0;
     } else {
       slideIndex++;
@@ -257,7 +337,7 @@ function bodyScrollingToggle() {
 
   // prev slide
   prevBtn.addEventListener("click", () => {
-    if(slideIndex === 0) {
+    if (slideIndex === 0) {
       slideIndex = slides.length - 1;
     } else {
       slideIndex--;
@@ -266,19 +346,29 @@ function bodyScrollingToggle() {
   });
 
   // active slider - translate sliderContainer
-  function slider(){
+  function slider() {
     // deactive existing active slide
-    sliderContainer.querySelector(".testi-item.active").classList.remove("active");
+    sliderContainer
+      .querySelector(".testi-item.active")
+      .classList.remove("active");
 
     // active next slide
     slides[slideIndex].classList.add("active");
 
     // translate sliderContainer
-    sliderContainer.style.transform = "translateX(" + -slideIndex * slideWidth + "px)";
-  }slider();
-
-
+    sliderContainer.style.transform =
+      "translateX(" + -slideIndex * slideWidth + "px)";
+  }
+  slider();
 })();
 
+/*----------------------- hide all sections except active -----------------------*/
 
-
+(() => {
+  const sections = document.querySelectorAll(".section");
+  sections.forEach((section) => {
+    if (!section.classList.contains("active")) {
+      section.classList.add("hide");
+    }
+  });
+})();
